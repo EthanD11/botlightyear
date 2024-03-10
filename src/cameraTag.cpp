636cacddbaa1f8e48c12dd9ed114b9e89cde7dd8
Bot Lightyear <scooby.dooby@doo.be>
2024-03-10 23:55:05 +0100

    update makefile to compile cpp files

--- src/cameraTag.cpp
+++ src/cameraTag.cpp
@@ -138,35 +138,35 @@ void tagDetectionValue(int* tag){
 }
 
 
-int main() {
-        test();
-/*
-    int tags[50];
-    tagDetectionValue(tags);
-    int size = tags[0];
-    if (size >50){
-        printf("size : %d \n", size);
-        size=50;
-    }
-    if (size!=0){
-        for (int i = 1; i <= size; ++i) {
-            printf("tag : %i ", tags[i]);
-        }
-    }
-    else{
-        printf("no tags detect\n");
-    }
+// int main() {
+//         test();
+// /*
+//     int tags[50];
+//     tagDetectionValue(tags);
+//     int size = tags[0];
+//     if (size >50){
+//         printf("size : %d \n", size);
+//         size=50;
+//     }
+//     if (size!=0){
+//         for (int i = 1; i <= size; ++i) {
+//             printf("tag : %i ", tags[i]);
+//         }
+//     }
+//     else{
+//         printf("no tags detect\n");
+//     }
     
-    /*int color = tagDetectionOrientation();
-    if (color==0){
-        printf("blue\n");
-    } else if (color==1){
-        printf("yellow\n");
-    } else if (color==10){
-        printf("blue and yellow\n");
-    } else {
-        printf("no tag\n");
-    }*/
+//     /*int color = tagDetectionOrientation();
+//     if (color==0){
+//         printf("blue\n");
+//     } else if (color==1){
+//         printf("yellow\n");
+//     } else if (color==10){
+//         printf("blue and yellow\n");
+//     } else {
+//         printf("no tag\n");
+//     }*/
     
-    return 0;
-}
+//     return 0;
+// }
--- src/lidar.cpp
+++ src/lidar.cpp
@@ -159,23 +159,23 @@ void StopLidar(){
 
 
 
-int main(int argc, char *argv[]) {
-   /*
-    ILidarDriver* lid = connectLidarL();
-    sleep(10);
-    disconnectLidarL(lid);
-    */
+// int main(int argc, char *argv[]) {
+//    /*
+//     ILidarDriver* lid = connectLidarL();
+//     sleep(10);
+//     disconnectLidarL(lid);
+//     */
     
     
     
-    StartLidar();
-    for (size_t i = 88; i < 92; i++)
-    {
-        sleep(10);
-        DataToFile("testBottom"+std::to_string(i)+".txt");
-        printf("turn\n");
-    }
+//     StartLidar();
+//     for (size_t i = 88; i < 92; i++)
+//     {
+//         sleep(10);
+//         DataToFile("testBottom"+std::to_string(i)+".txt");
+//         printf("turn\n");
+//     }
     
-    StopLidar();
+//     StopLidar();
     
-}
+// }
