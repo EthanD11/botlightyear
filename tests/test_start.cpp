7210e95cd5d2380eab5057de933c4234dbacb7e3
ageorgopoulos <alexandra.georgopoulos@outlook.com>
2024-03-23 11:21:11 +0100

    main + test start

--- tests/test_start.cpp
+++ tests/test_start.cpp
@@ -5,6 +5,15 @@ int main(int argc, char const *argv[])
     //Init Starting Switch
     int handle = lgGpiochipOpen(0);
     if (handle < 0) exit(1);
+    int status = lgGpioGetMode(0, 4); 
+    if (status < 0) {
+        printf("Cannot get GPIO mode \n");
+        exit(1);
+    }
+    else {
+        printf("GPIO mode: %d \n", status);
+    }
+
     if (lgGpioClaimInput(handle, 0, 4) != 0) {
         printf("Cannot set GPIO to input \n");
         exit(1);
@@ -13,8 +22,9 @@ int main(int argc, char const *argv[])
     printf("Initial value of GPIO: %d \n", start);
 
     printf("Waiting start of the game... \n");
-    int i = 0;
+
     //Wait for starting switch
+    int i = 0;
     while (start == 0) {
         start = lgGpioRead(handle, 4);
         printf("Waiting %d, GPIO %d \n", i, start); 
