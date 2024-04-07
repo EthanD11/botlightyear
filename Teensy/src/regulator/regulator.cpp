07a329b512c483b2342bd7c6080f041db72d091c
EthanDecleyn <ethan.decleyn@student.uclouvain.be>
2024-04-07 15:10:40 +0200

    Mise à jour des gains du régulateur et du path following 2

--- Teensy/src/regulator/regulator.cpp
+++ Teensy/src/regulator/regulator.cpp
@@ -10,7 +10,7 @@ Regulator *init_regulator() {
     reg->ki = 44; // Former t1_ki
     
 
-    reg->imax = 2; // Anti windup [V/V]
+    reg->imax = 1; // Anti windup [V/V]
 
     reg->isl = 0.0; // left integral state
     reg->isr = 0.0; // right integral state
