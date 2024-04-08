c140d87488fb78370eaa3ffb74adca6a7e8c417e
Ethan Decleyn <ethan.decleyn@student.uclouvain.be>
2024-04-09 00:15:31 +0200

    Test servos tweak

--- tests/test_servos.cpp
+++ tests/test_servos.cpp
@@ -16,8 +16,8 @@ int main(int argc, char const *argv[])
     /*flaps_servo_cmd(FlapsDeploy);
     lguSleep(3);
     flaps_servo_cmd(FlapsRaise);
-    lguSleep(1);
-    flaps_servo_cmd(FlapsIdle);*/
+    lguSleep(1);*/
+    flaps_servo_cmd(FlapsIdle);
     
     /*gripper_holder_cmd(HolderClosed);
     lguSleep(3);
@@ -26,16 +26,16 @@ int main(int argc, char const *argv[])
     gripper_holder_cmd(HolderPlant);
     lguSleep(3);
     gripper_holder_cmd(HolderPot);
-    lguSleep(1);
-    gripper_holder_cmd(HolderIdle);*/
+    lguSleep(1);*/
+    gripper_holder_cmd(HolderIdle);
 
     //gripper_deployer_cmd(DeployerDeploy);
     //lguSleep(3);
     //gripper_deployer_cmd(DeployerHalf);
     //lguSleep(3);
-    gripper_deployer_cmd(DeployerRaise);
+    //gripper_deployer_cmd(DeployerRaise);
     //lguSleep(1);
-    //gripper_holder_cmd(HolderIdle);
+    gripper_deployer_cmd(DeployerIdle);
 
     close_spi();
     return 0;
