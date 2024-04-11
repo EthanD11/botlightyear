c1c788ba577fbe3814143759f4702537cce6d1c2
ageorgopoulos <alexandra.georgopoulos@outlook.com>
2024-04-11 22:48:11 +0200

    test action sp rework with new spi

--- tests/test_move_and_sp.cpp
+++ tests/test_move_and_sp.cpp
@@ -1,19 +1,27 @@
-#include "../headers/SPI_Modules.h"
+#include "teensy.h"
+#include "odometry.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <cmath>
+#include <lgpio.h>
+
+SPIBus spiBus = SPIBus();
+GPIOPins pins = GPIOPins(); 
+Teensy teensy = Teensy(&spiBus, &pins);
+Odometry odo = Odometry(&spiBus);
 
 const double deg_to_rads = M_PI/180;
 
 int main(int argc, char const *argv[])
 {
-    init_spi(); 
-    lguSleep(0.5);
+    usleep(500000);
 
     // Set position control gains
     double kp = 0.8;
     double ka = 2.5;
     double kb = -0.5;
     double kw = 4.0;
-    teensy_set_position_controller_gains(kp, ka, kb, kw);
+    teensy.set_position_controller_gains(kp, ka, kb, kw);
 
     // Set path follower gains
     double kt = 2.0;
@@ -24,7 +32,7 @@ int main(int argc, char const *argv[])
     double epsilon = M_PI/8; // epsilon is in radians
     double wn = 0.2; // Command filter discrete cutoff frequency
     double kv_en = 0.;
-    teensy_set_path_following_gains(kt, kn, kz, sigma, epsilon, kv_en, delta, wn);
+    teensy.set_path_following_gains(kt, kn, kz, sigma, epsilon, kv_en, delta, wn);
 
     // Define trajectory
     int ncheckpoints = 2;
@@ -36,21 +44,19 @@ int main(int argc, char const *argv[])
 
     // Set teensy and odometry starting positions
     double xpos, ypos, thetapos;
-    odo_set_pos(x[0], y[0], theta_start);
-    teensy_set_position(x[0], y[0], theta_start);
+    odo.set_pos(x[0], y[0], theta_start);
+    teensy.set_position(x[0], y[0], theta_start);
     
     // Orientation with position control
-    teensy_pos_ctrl(x[1], y[1], theta_end);
+    teensy.pos_ctrl(x[1], y[1], theta_end);
     lguSleep(0.1);
 
     // Reset teensy estimated position with odometry
     while (true) {
-        odo_get_pos(&xpos, &ypos, &thetapos);
-        teensy_set_position(xpos, ypos, thetapos);
+        odo.get_pos(&xpos, &ypos, &thetapos);
+        teensy.set_position(xpos, ypos, thetapos);
         lguSleep(0.3);
     }
 
-
-    close_spi();
     return 0;
 }
