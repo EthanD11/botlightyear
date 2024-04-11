e94b047c2a7cd5695c2a24226398970347dd60db
Ethan Decleyn <ethan.decleyn@student.uclouvain.be>
2024-04-11 12:38:42 +0200

    Path following working git status

--- tests/test_teensy.cpp
+++ tests/test_teensy.cpp
@@ -24,53 +24,75 @@ int main(int argc, char const *argv[])
     lguSleep(0.5);
 
     #ifdef POSITION_CONTROL
+    double kp = 1.0;
+    double ka = 4.0;
+    double kb = -1.5;
+    double kw = 5.0;
+    teensy_set_position_controller_gains(kp, ka, kb, kw);
+
     double x = 0; 
     double y = 0; 
     double t = 0;
-    double xr = 0.2; 
-    double yr = 0; 
-    double tr = -60*deg_to_rads;
+    double xr = 0.5; 
+    double yr = 0.1; 
+    double tr = 30*deg_to_rads;
+    double xpos, ypos, thetapos;
 
     teensy_set_position(x, y, t);
+    odo_set_pos(x, y, t);
+    lguSleep(0.1);
     teensy_pos_ctrl(xr, yr, tr);
+
+    lguSleep(0.1);
+    while (true) {
+        odo_get_pos(&xpos, &ypos, &thetapos);
+        teensy_set_position(xpos, ypos, thetapos);
+        lguSleep(0.1);
+    }
     #endif
 
     #ifdef PATH_FOLLOWING
-    double kt = 1.0;
-    double kn = 0.32; // 0 < kn <= 1
-    double kz = 20.0;
+    double kp = 0.8;
+    double ka = 2.5;
+    double kb = -0.8;
+    double kw = 4.0;
+    teensy_set_position_controller_gains(kp, ka, kb, kw);
+
+    double kt = 2.0;
+    double kn = 0.3; // 0 < kn <= 1
+    double kz = 25.0;
     double delta = 20e-3; // delta is in meters
-    double sigma = 0.0;
+    double sigma = 10;
     double epsilon = M_PI/8; // epsilon is in radians
     double wn = 0.2; // Command filter discrete cutoff frequency
-    double kv_en = 12;
+    double kv_en = 0.;
     teensy_set_path_following_gains(kt, kn, kz, sigma, epsilon, kv_en, delta, wn);
     lguSleep(0.1);
-    int ncheckpoints = 2;
-    // double x[5] = {0.1,0.5,0.9,0.5,0.1};
-    // double y[5] = {1.5,1.7,1.5,1.3,1.5};
-    double x[5] = {0.1,1.0};
-    double y[5] = {1.5,1.5};
+    int ncheckpoints = 5;
+    double x[5] = {0.1,0.5,0.9,0.5,0.1};
+    double y[5] = {1.5,1.7,1.5,1.3,1.5};
+    // double x[5] = {0.1,1.2};
+    // double y[5] = {1.5,1.5};
     double theta_start = 0.;
-    double theta_end = 0;
-    double vref = 0.1;
-    double dist_goal_reached = 0.2;
+    double theta_end = M_PI;
+    double vref = 0.25;
+    double dist_goal_reached = 0.25;
 
     double xpos, ypos, thetapos;
-    odo_set_pos(0.1, 1.5, 0);
+    odo_set_pos(x[0], y[0], 0);
     lguSleep(0.5);
-    teensy_set_position(0.1, 1.5, 0);
-    lguSleep(2);
+    teensy_set_position(x[0], y[0], 0);
+    lguSleep(1);
 
-    teensy_pos_ctrl(0.1, 1.5, atan2(y[1]-y[0], x[1]-x[0]));
+    teensy_pos_ctrl(x[0], y[0], atan2(y[1]-y[0], x[1]-x[0]));
     lguSleep(2);
 
     teensy_path_following(x, y, ncheckpoints, theta_start, theta_end, vref, dist_goal_reached);
-    lguSleep(0.3);
+    lguSleep(0.1);
     while (true) {
         odo_get_pos(&xpos, &ypos, &thetapos);
-        lguSleep(0.5);
         teensy_set_position(xpos, ypos, thetapos);
+        lguSleep(0.3);
     }
     #endif
 
