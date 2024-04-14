c8d41dd864f23cb856cde55da2cf1a80022e8b1a
ageorgopoulos <alexandra.georgopoulos@outlook.com>
2024-04-14 16:21:12 +0200

    actions utils: path follow, pos control

--- headers/actions.h
+++ headers/actions.h
@@ -9,4 +9,10 @@
 #include "action_SP.h"
 #include "action_zone.h"
 
+/* UTILS: PATH_FOLLOWING_TO_ACTION */
+void path_following_to_action(path_t *path); 
+
+/* UTILS: POSITION_CONTROL */
+void action_position_control(double x_end, double y_end, double theta_end); 
+
 #endif
---
+++ src/actions.cpp
@@ -0,0 +1,93 @@
+#include "actions.h"
+
+void path_following_to_action(path_t *path) {
+
+    Teensy *teensy = shared.teensy; 
+
+    // Set path following from path planning (decision)
+    int ncheckpoints = (int) path->nNodes; 
+    double *x = path->x; 
+    double *y = path->y; 
+
+    double theta_start = path->thetaStart; 
+    double theta_end = path->thetaEnd; 
+
+    double kp = 0.8;
+    double ka = 2.5;
+    double kb = -0.8;
+    double kw = 4.0;
+    teensy->set_position_controller_gains(kp, ka, kb, kw);
+
+    double kt = 2.0;
+    double kn = 0.3; // 0 < kn <= 1
+    double kz = 25.0;
+    double delta = 20e-3; // delta is in meters
+    double sigma = 10;
+    double epsilon = M_PI/8; // epsilon is in radians
+    double wn = 0.2; // Command filter discrete cutoff frequency
+    double kv_en = 0.;
+    teensy->set_path_following_gains(kt, kn, kz, sigma, epsilon, kv_en, delta, wn);
+
+    teensy->path_following(x, y, ncheckpoints, theta_start, theta_end, vref, dist_goal_reached);
+
+    // Check Teensy mode
+    //usleep(100000);
+    while ((teensy->ask_mode()) != ModePositionControlOver) {
+        usleep(1000);
+    } 
+
+}
+
+void action_position_control(double x_end, double y_end, double theta_end) {
+
+    Teensy *teensy = shared.teensy;
+    Odometry *odo = shared.odo; 
+
+    // Retrieve robot current position
+    double x, y, theta; 
+    shared.get_robot_pos(&x, &y, &theta); 
+    #ifdef VERBOSE
+    printf("Robot position from shared: %.3f, %.3f, %.3f \n", x, y, theta); 
+    #endif
+
+    // Retrieve adversary current position
+    double x_adv, y_adv, theta_adv; 
+    shared.get_adversary_pos(&x_adv , &y_adv, &theta_adv); 
+    #ifdef VERBOSE
+    printf("Adversary position from shared: %.3f, %.3f, %.3f \n", x_adv, y_adv, theta_adv); 
+    #endif
+
+    // Check adversary
+    
+
+
+    // Set position control gains (see with Ethan?)
+    double kp = 0.8;
+    double ka = 2.5;
+    double kb = -0.5;
+    double kw = 4.0;
+    teensy->set_position_controller_gains(kp, ka, kb, kw);
+
+    // Define trajectory
+    int ncheckpoints = 2;
+    double xc[2] = {x, x_end};
+    double yc[2] = {y, y_end};
+    double theta_start = theta;
+    double theta_end = theta_end;
+    
+    #ifdef VERBOSE
+    printf("Checkpoints relay: %.3f, %.3f\n", xc[0], yc[0]); 
+    printf("Checkpoints target: %.3f, %.3f\n", xc[1], yc[1]); 
+    #endif
+    
+    // Orientation with position control
+    teensy->pos_ctrl(xc[1], yc[1], theta_end);
+    lguSleep(0.1);
+
+    // Waiting end to start function turn_solar_panel
+    // Check Teensy mode
+    while ((teensy->ask_mode()) != ModePositionControlOver) { 
+        usleep(10000);
+    } 
+
+}
