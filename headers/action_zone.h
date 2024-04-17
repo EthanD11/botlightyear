033c4428132a50aa02348bd91c4b404bc7a501e0
MartinBrans <75911369+MartinBrans@users.noreply.github.com>
2024-04-17 23:07:53 +0200

    Action zone, action return

--- headers/action_zone.h
+++ headers/action_zone.h
@@ -3,4 +3,15 @@
 
 #include "actions.h"
 
+class ActionZone : public Action
+{
+private:
+    uint8_t nbPlants; // Number of plants to drop
+public:
+    ActionZone(graph_path_t *path, uint8_t nbPlants) : Action(DepositPlanter, true, path) {
+        this->nbPlants = nbPlants;
+    }
+    ~ActionZone() {}
+    void do_action();
+};
 #endif
