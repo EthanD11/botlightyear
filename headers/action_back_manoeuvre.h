35a7298ed5582f331a2893d9f30a82a61b76dd69
EthanDecleyn <ethan.decleyn@student.uclouvain.be>
2024-04-18 13:05:09 +0200

    Implementation of BACK manoeuver

--- headers/action_back_manoeuvre.h
+++ headers/action_back_manoeuvre.h
@@ -7,7 +7,22 @@
 
 class ActionBACK : public Action { 
     private:
-        double xnew, ynew, thetanew; 
+        double xnew, ynew, thetanew;
+        double const safety_distance_to_walls = 0.2; // [m]
+        double const safety_distance_to_pami_side_wall = 0.3; 
+        double const dist_useless = 0.05;
+        bool manoeuver_useless;
+        bool too_close_to_wall(double x, double y) {
+            if ((x < safety_distance_to_pami_side_wall) ||
+                (y < safety_distance_to_walls) ||
+                (2.0 - x < safety_distance_to_walls) ||
+                (3.0 - y < safety_distance_to_walls)
+                ) {
+                return true;
+            }
+            return false;
+        }
+
     public: 
 
         /**
@@ -17,13 +32,30 @@ class ActionBACK : public Action {
          * @param backward_dist The distance botlightyear will move backward
          * @param angular_displacement: The angular displacement that botlightyear will realise during the back manoeuver
         */
-        ActionBACK (double backward_dist, double angular_displacement) : Action(BackManoeuvre, false, NULL) { 
-            // Compute position from robot current position ang specified angular displacement
+        ActionBACK (double backward_dist) : Action(BackManoeuvre, false, NULL) { 
+            // 1) Get position
+            // 2) While new position too close to wall, reduce backward dist
             double xpos, ypos, thetapos;
             shared.get_robot_pos(&xpos, &ypos, &thetapos);
-            this->xnew = xpos - cos(thetapos)*backward_dist;
-            this->ynew = ypos - sin(thetapos)*backward_dist;
-            this->thetanew = thetapos + angular_displacement;
+
+            double xnew, ynew, thetanew;
+            xnew = xpos - cos(thetapos)*backward_dist;
+            ynew = ypos - sin(thetapos)*backward_dist;
+            thetanew = thetapos;
+
+            while (this->too_close_to_wall(xnew, ynew) && (backward_dist > dist_useless)) {
+                backward_dist = backward_dist - 0.01;
+                xnew = xpos - cos(thetapos)*backward_dist;
+                ynew = ypos - sin(thetapos)*backward_dist;
+            }
+
+            if (backward_dist <= dist_useless) {
+                this->manoeuver_useless = true;
+            } else {
+                this->manoeuver_useless = false;
+            }
+
+
         }
         ~ActionBACK() {};
         void do_action();
--- src/action_back_manoeuvre.cpp
+++ src/action_back_manoeuvre.cpp
@@ -1,17 +1,27 @@
 #include "action_back_manoeuvre.h"
 
 void ActionBACK::do_action(){
-    // 1) Initiate pos control gains
-    shared.teensy->set_position_controller_gains(
-        0.8, // kp
-        8.0, // ka
-        2.0, // kb
-        6.0 // kw
-    );
+    if (!this->manoeuver_useless) {
+        // 1) Initiate pos control gains
+        shared.teensy->set_position_controller_gains(
+            0.8, // kp
+            8.0, // ka
+            2.0, // kb
+            6.0 // kw
+        );
 
-    // 2) Initiate position control to computed position
-    shared.teensy->pos_ctrl(
-        this->xnew, 
-        this->ynew, 
-        this->thetanew);
+        // 2) Initiate position control to computed position
+        shared.teensy->pos_ctrl(
+            this->xnew, 
+            this->ynew, 
+            this->thetanew);
+        
+        // 3) Wait for position control to be over
+        while ((shared.teensy->ask_mode()) != ModePositionControlOver)
+        {
+            usleep(10000);
+        }
+    }
+
+    
 }
