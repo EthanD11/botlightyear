729570f19bc6c5daafc43ad52c1c2ea7a45acf16
Bot Lightyear <botlightyear@gmail.com>
2024-04-19 19:31:26 +0200

    Luma go away plz python

--- luma.oled-1.5.0/luma.oled-1.5.0/build/lib/oled/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2016 Richard Hull and contributors
-# See LICENSE.rst for details.
-
-"""
-OLED display driver for SSD1306, SSD1325, SSD1331 and SH1106 devices.
-"""
-
-__version__ = '1.5.0'
