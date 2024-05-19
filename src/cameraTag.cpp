42d89affb5e046e8eef270656fc9bf04377495de
phanssens <100869295+phanssens@users.noreply.github.com>
2024-05-19 12:36:40 +0200

    Comment cameraTag.cpp

--- src/cameraTag.cpp
+++ src/cameraTag.cpp
@@ -11,6 +11,7 @@ using namespace cv;
 
 
 double tagSolar(){
+    //On raspberry 5, the packages are not yet up to date, so you need to manually take a photo to be able to analyse it. 
     system("libcamera-still  -o tag0.jpg –camera 0 -t 1 -v 0 ");
     cv::Mat image = cv::imread("tag0.jpg");
     if (image.empty()) {
@@ -27,11 +28,12 @@ double tagSolar(){
     //Detection ARUCO
     cv::aruco::detectMarkers(image, dictionary, markerCorners, markerIds, parameters);
 
-    //Calibration matrix
+    //Calibration matrix (calculated from tests performed)
     Mat cameraMatrix,  distCoeffs;    
     cameraMatrix = (Mat1f(3, 3) << 2013.1301, 0, 719.5, 0, 2013.1301, 539.5, 0, 0, 1);
     distCoeffs = (Mat1f(4, 1) <<-0.236433, 0.0344246, 0, 0, 0);
     double a1, a2,a3,d1, d2,d3, d;
+    
     //Orientation
     double dtot=100;
     if (markerIds.size() > 0) {
@@ -39,7 +41,7 @@ double tagSolar(){
         aruco::estimatePoseSingleMarkers(markerCorners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
         for (size_t i = 0; i < markerIds.size(); ++i) {
             if (markerIds[i]==47&&(abs(tvecs[i][0])<abs(dtot))){
-                //panneau solaire
+                //Data recovery and angle modulation between Pi and -Pi
                 a1 = rvecs[i][0];
                 a2 = rvecs[i][1];
                 a3 = rvecs[i][2];
@@ -51,12 +53,14 @@ double tagSolar(){
                 a1 = std::fmod((a1 +M_PI), (2*M_PI))-M_PI;
                 a2 = std::fmod((a2 +M_PI), (2*M_PI))-M_PI;
                 a3 = std::fmod((a3 +M_PI), (2*M_PI))-M_PI;
-                //printf("0\n %.2f\n %.2f\n %.2f\n %.2f\n %.2f\n %.2f\n\n",a1*180/M_PI, a2*180/M_PI,a3*180/M_PI, d1, d2, d3);
             }
         }
     }
-    a1= a1*180.0/M_PI;
+    // conversion of angles into degrees
+    a1 = a1*180.0/M_PI;
     a2 = a2*180.0/M_PI;
+
+    //Linear regression based on tests
     if (a1>a2){
         double angle = (a1+260.3)/(-1.29);
         while (angle>180){
